3ab47ef4f2b77dac24a7832115423b0ecd0571c0
kamal Hosseini <kamalhoseini68@gmail.com>
2023-01-27 11:15:30 +0100

    add chunck

--- create_dataset.py
+++ create_dataset.py
@@ -1,19 +1,32 @@
 
 
 
-def frquency_corpus(list_of_string):
-    freq = {}
-    for i in  list_of_string:
-        for j in i:
-
-            freq[j] = freq.get(j, 0) +1
-    return freq
 
 
+def count_word(list_word, sort = True):
+    count = {}
+    for i in list_word:
+        count[i] = count.get(i, 0) + 1
+    if sort:
+        count = dict(sorted(count.items(), key = lambda item : item[1], reverse = True))
+    return count
 
 
 
 
+
+def chunk(sequence, batch, stride, drop_last = False):
+    buffer = []
+    for i in sequence:
+        buffer.append(i)
+        if len(buffer) == batch:
+            yield buffer
+            buffer= buffer[stride:]
+    if (not drop_last):
+        yield buffer
+        
+        
+        
 def count_n_gram1(sentence, n_gram = 3, stride = 1):
 
     count = {}
